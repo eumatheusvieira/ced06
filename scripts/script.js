ea026255ca089950e35e240fe91431109e0696f4
Matheus Vieira <matheusvrgo2@gmail.com>
2024-04-18 22:04:57 -0300

    ADICIONAR NOVO SLIDER E FOOTER

--- scripts/script.js
+++ scripts/script.js
@@ -1,44 +1,10 @@
-const slide = document.querySelectorAll(".slide");
-const btnPrev = document.getElementById("prev-btn");
-const btnNext = document.getElementById("next-btn");
+
 const showPopup = document.querySelector(".sm1");
 const showPopup2 = document.querySelector(".sm2");
 const hidePopup = document.querySelector(".saibamenos");
 const hidePopup2 = document.querySelector(".saibamenos2");
 const popupContainer = document.querySelector(".sm-container");
 const popupContainer2 = document.querySelector(".sm-container2");
-
-let currentSlide = 0;
-
-function hideSlide() {
-  slide.forEach((item) => item.classList.remove("on"));
-}
-function showSlide() {
-  slide[currentSlide].classList.add("on");
-}
-
-function nextSlide() {
-  hideSlide();
-  if (currentSlide == slide.length - 1) {
-    currentSlide = 0;
-  } else {
-    currentSlide++;
-  }
-  showSlide();
-}
-
-function prevSlide() {
-  hideSlide();
-  if (currentSlide == 0) {
-    currentSlide = slide.length - 1;
-  } else {
-    currentSlide--;
-  }
-  showSlide();
-}
-
-btnNext.addEventListener("click", nextSlide);
-btnPrev.addEventListener("click", prevSlide);
 
 showPopup.onclick = () => {
   popupContainer.classList.add("active");
@@ -67,3 +33,13 @@ hidePopup.onclick = () => {
       document.querySelector('.icon').src = "images/close-bold-svgrepo-com.svg"
     }
   }
+
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
